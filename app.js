c181fdec70880b1f8256bc4722b0f5ff522653ea
Nielll951 <rodina9180gaocheng@ukr.net>
2023-11-28 15:00:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// find popup
+let popup = document.getElementById("popup")
+// let popup = document.getElementById("popup")
+
+// this function will open the popup
+function openPopup() {
+   popup.classList.add("open-popup")
+}
+
+// This function will close popup
+function closePopup() {
+   popup.classList.remove('open-popup')
+}
